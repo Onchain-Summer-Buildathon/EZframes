85de0fc573574a32a9f1afc5dce3fc265e3aded3
Bhavya Gor <bhavya.gor9999@gmail.com>
2024-09-27 16:34:18 +0530

    all apis fix

--- packages/nextjs/app/api/transactions/[id]/route.ts
+++ packages/nextjs/app/api/transactions/[id]/route.ts
@@ -2,10 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import Transaction from "~~/model/transaction";
 import connectDB from "~~/services/connectDB";
 
+// all of this is based on frame_id
+
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
-  const transaction_id = params.id;
-  const transaction = await Transaction.findById(transaction_id);
+  const frame_id = params.id;
+  const transaction = await Transaction.findOne({ frameId: frame_id });
   if (!transaction) {
     return new NextResponse(JSON.stringify({ message: "transaction not found" }), { status: 404 });
   }
@@ -14,9 +16,9 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
 
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
-  const transaction_id = params.id;
+  const frame_id = params.id;
   const payload = await req.json();
-  const transaction = await Transaction.findByIdAndUpdate(transaction_id, payload, { new: true });
+  const transaction = await Transaction.updateOne({ frameId: frame_id }, payload, { new: true });
   if (!transaction) {
     return new NextResponse(JSON.stringify({ message: "transaction not found" }), { status: 404 });
   }
@@ -25,8 +27,8 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
-  const transaction_id = params.id;
-  const transaction = await Transaction.findByIdAndDelete(transaction_id);
+  const frame_id = params.id;
+  const transaction = await Transaction.deleteOne({ frameId: frame_id });
   if (!transaction) {
     return new NextResponse(JSON.stringify({ message: "transaction not found" }), { status: 404 });
   }
